04c7df7d4c5e90a2351030abf6bd1ab2d32fa3d9
Ahmet Cefakar <sabit.durdu@yandex.com>
2019-04-16 21:19:51 +0300

    Add project files.

---
+++ WebAppSchecular/Global.asax.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Quartz;
+using Quartz.Impl;
+
+namespace WebAppSchecular
+{
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+			Scheduler.Execute();
+		}
+	}
+
+	public class Scheduler
+	{
+		public static void Execute()
+		{
+			// construct a scheduler factory
+			ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+			// get a scheduler, start the schedular before triggers or anything else
+			IScheduler sched = schedFact.GetScheduler();
+			sched.Start();
+
+			// create job
+			IJobDetail job = JobBuilder.Create<SimpleJob>()
+					.WithIdentity("job1", "group1")
+					.Build();
+
+			// create trigger
+			ITrigger trigger = TriggerBuilder.Create()
+				.WithIdentity("trigger1", "group1")
+				.WithSimpleSchedule(x => x.WithIntervalInSeconds(5).RepeatForever())
+				.Build();
+
+			// Schedule the job using the job and trigger 
+			sched.ScheduleJob(job, trigger);
+		}
+
+	}
+
+	public class SimpleJob : IJob
+	{
+		void IJob.Execute(IJobExecutionContext context)
+		{
+			Debug.WriteLine("Scheduler Runned!");
+		}
+	}
+}
