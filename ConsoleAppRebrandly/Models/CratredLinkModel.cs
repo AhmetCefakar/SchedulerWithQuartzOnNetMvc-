adf9f0ae002737c73434723104d1a53dc80b72fd
Ahmet <sorswaretablet@gmail.com>
2019-04-22 15:22:45 +0300

    Rebrandly kullanım örneği eklendi.

---
+++ ConsoleAppRebrandly/Models/CratredLinkModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRebrandly.Models
+{
+    public class CratredLinkModel
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string slashtag { get; set; }
+        public string destination { get; set; }
+        public string createdAt { get; set; }
+        public string updatedAt { get; set; }
+        public string status { get; set; }
+        public string[] tags { get; set; }
+        public int clicks { get; set; }
+        public bool isPublic { get; set; }
+        public string shortUrl { get; set; }
+        public string domainId { get; set; }
+        public string domainName { get; set; }
+        public bool https { get; set; }
+        public bool favourite { get; set; }
+        public bool integrated { get; set; }
+
+        public object domain { get; set; }
+        public object creator { get; set; }
+
+    }
+}
---
+++ ConsoleAppRebrandly/Program.cs
@@ -0,0 +1,59 @@
+﻿using ConsoleAppRebrandly.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRebrandly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> urlLinks = new List<string>
+            {
+                "http://yazilimustam.com/c-webclient-ve-httpclient-kullanimi/",
+                //"https://ademocut.com/kisaltilmis-url-nasil-yapilir-google-url-shortener-ademocut-com/",
+                //"https://www.youtube.com/results?search_query=Firebase+Dynamic+Links+API+Client+Library+for+.NET+examples"
+            };
+
+            List<string> urlShortdLinks = new List<string>();
+
+            for (int i = 0; i < urlLinks.Count; i++)
+            {
+                WebClient client = new WebClient();
+                client.Headers.Add("Content-Type", "application/json");
+                client.Headers.Add("apikey", "7d8021cb454846438863377755dc258a");
+                //client.Headers.Add("workspace", "Main_Workspace");
+
+                string body = "{\"destination\":\"" + urlLinks[i] + "\",\"domain\":{\"fullName\":\"rebrand.ly\"}}"; // https://www.youtube.com/channel/UCHK4HD0ltu1-I212icLPt3g
+
+                try
+                {
+                    string response = client.UploadString("https://api.rebrandly.com/v1/links", "POST", body); // API'ya link kısaltmak için istek yapılıyor
+
+
+                    CratredLinkModel cratredLinkModel = JsonConvert.DeserializeObject<CratredLinkModel>(response);
+                    urlShortdLinks.Add(cratredLinkModel.shortUrl);
+                }
+                catch (Exception ex)
+                {
+                    //request failed
+                }
+            }
+
+
+            Console.WriteLine("==============Oluşturulan kısa linkler;===============");
+            for (int i = 0; i < urlShortdLinks.Count; i++)
+            {
+                Console.WriteLine($"Uzun link: {urlLinks[i]}, Kısalink: {urlShortdLinks[i]}");
+            }
+
+            Console.WriteLine("=============================");
+            Console.ReadLine();
+        }
+    }
+}
